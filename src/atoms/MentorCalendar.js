6e69120dd26d6957b123cbbd0b75aaa88a97ef72
qasim <qasim@primzel.com>
2025-03-07 16:53:14 +0500

    feat: added drag/drop event update

--- src/atoms/MentorCalendar.js
+++ src/atoms/MentorCalendar.js
@@ -1,17 +1,47 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment'
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
 
 import './mentor-calendar.scss'
+import {useState} from "react";
 
 const localizer = momentLocalizer(moment)
+const DnDCalendar = withDragAndDrop(Calendar);
+
 
 const MentorCalendar = () => {
+    const [state, setData] = useState({
+        events: [
+            {
+                start: moment().toDate(),
+                end: moment().toDate(),
+                title: "Qasim/Sajnay Call",
+            }
+        ],
+    });
+    const onEventDrop = (data) => {
+        const {start, end} = data;
+        state.events[0].start = start;
+        state.events[0].end = end;
+        setData({ events: [...state.events] });
+        console.log(start,end)
+    };
+    const onEventResize = (data) => {
+        const {start, end} = data;
+        state.events[0].start = start;
+        state.events[0].end = end;
+        setData({ events: [...state.events] });
+    };
     return (
         <div className="mentor-calendar-placeholder">
-            <Calendar
+            <DnDCalendar
+                defaltView="weeks"
                 localizer={localizer}
-                startAccessor="start"
-                endAccessor="end"
+                onEventDrop={onEventDrop}
+                onEventResize={onEventResize}
+                events={state.events}
+                resizable
             />
         </div>
     );
